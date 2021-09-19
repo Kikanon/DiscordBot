5fef4dec435bb3a9fa96e910f30917179a9e5ef4
Gregor <jakob.srumpf@gmail.com>
2021-09-20 00:05:35 +0200

    prefix, nickname, config

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ const Discord = require('discord.js');
 const Puppeteer = require('puppeteer');
 const Canvas = require('canvas');
 const JsBarcode = require('jsbarcode');
+const Fs = require('fs');
+
+
 const MAX_CHROME_PAGES = 2;
-let PREFIX = ">";
+const DEAFULT_NICKNAME = '';
+const DEAFULT_GUILD_DATA = {prefix:'>', nickname:'mr. bot', guild_name:''};
+
+
 let CHROME_PAGES = 0;
+let GUILDS_DATA = {};
+
+
 // dw bout it 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
 const token = "MzY4NDIyNTU3ODgxMzM1ODEw.WeDePg.263kHD8VH8zxpc2EAl-88bNtRhI";
 
@@ -15,7 +24,7 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 process.setMaxListeners(100);
 
 async function genPetrolCode() {
-  const browser = await Puppeteer.launch({ headless: true, executablePath: "/usr/bin/chromium" });
+  const browser = await Puppeteer.launch({ headless: true }); // linux: executablePath: "/usr/bin/chromium"
 
   const page = await browser.newPage();
   page.setDefaultTimeout(300000);
@@ -45,10 +54,54 @@ async function genPetrolCode() {
   return imeNagrade + " : " + code;
 }
 
+async function saveConfig() {
+  let data = JSON.stringify(GUILDS_DATA, null, 2);
+  Fs.writeFileSync('guilds_data.json', data);
+}
+
 //-------------------- BOT READY --------------------
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log('Bot ready');
-  client.user.setActivity('Mind ur own biz')
+
+  try {
+    let rawdata = Fs.readFileSync('guilds_data.json');
+    GUILDS_DATA = JSON.parse(rawdata);
+  }
+  catch(e)
+  {
+    GUILDS_DATA = {}
+  }
+
+  client.guilds.cache.forEach(guild =>{
+    if(GUILDS_DATA[guild.id] == undefined)
+    {
+      GUILDS_DATA[guild.id] = Object.assign({},DEAFULT_GUILD_DATA);
+    }
+    GUILDS_DATA[guild.id].guild_name = guild.name;
+    guild.me.setNickname(GUILDS_DATA[guild.id].nickname + `[${GUILDS_DATA[guild.id].prefix}]`);
+  });
+
+  await saveConfig();
+  client.user.setActivity('Mind ur own biz');
+});
+
+client.on("guildCreate", async guild => {
+  console.log("Joined a new guild: " + guild.name);
+
+  GUILDS_DATA[guild.id] = Object.assign({},DEAFULT_GUILD_DATA);
+  guild.me.setNickname(GUILDS_DATA[guild.id].nickname + `[${GUILDS_DATA[guild.id].prefix}]`);
+
+  GUILDS_DATA[guild.id].guild_name = guild.name;
+
+  await saveConfig();
+});
+
+client.on("guildDelete", async guild => {
+  console.log("Left a guild: " + guild.name);
+  
+  GUILDS_DATA[guild.id] = undefined;
+
+  await saveConfig();
 });
 
 //-------------------- BOT FUNCTIONS --------------------
@@ -58,33 +111,59 @@ client.on("messageCreate", async message => {
   }
 });
 
+// HELP
 client.on("messageCreate", async message => {
-  if (String(message.content).startsWith(PREFIX + 'help')) {
+  if (String(message.content).startsWith(GUILDS_DATA[message.guild.id].prefix + 'help')) {
 
     let helpText = "commands: \n \
     \t-help \n \
     \t-hackPetrol [num]\n \
     \t-barcode [code]\n \
-    \t-setPrefix [prefix]"
+    \t-setPrefix [prefix]\n \
+    \t-setNickname [nickname]"
 
     message.channel.send(helpText);
   }
 });
 
 client.on("messageCreate", async message => {
-  if (String(message.content).startsWith(PREFIX + 'setPrefix')) {
+  if (String(message.content).startsWith(GUILDS_DATA[message.guild.id].prefix + 'setPrefix')) {
 
     let pref = String((message.content.split(' ')[1]) || "");
     if (pref=='') return;
 
-    PREFIX = pref;
+    message.guild.me.setNickname( GUILDS_DATA[message.guild.id].nickname + ` [${pref}]`);
 
-    message.channel.send(`New prefix is now \'${PREFIX}\'`);
+    GUILDS_DATA[message.guild.id].prefix = pref;
+
+    message.channel.send(`New prefix is now \'${pref}\'`);
+
+    await saveConfig();
   }
 });
 
 client.on("messageCreate", async message => {
-  if (String(message.content).startsWith(PREFIX + 'barcode')) {
+  if (String(message.content).startsWith(GUILDS_DATA[message.guild.id].prefix + 'setNickname')) {
+
+    let spaceIndex = message.content.indexOf(' ');
+
+    if(spaceIndex == -1)
+      return;
+
+    let nickname = String(message.content.substr(spaceIndex + 1).trim());
+
+    message.guild.me.setNickname( nickname + ` [${GUILDS_DATA[message.guild.id].prefix}]`);
+
+    GUILDS_DATA[message.guild.id].nickname = nickname;
+
+    message.channel.send(`My nickname is now \'${nickname}\'`);
+
+    await saveConfig();
+  }
+});
+
+client.on("messageCreate", async message => {
+  if (String(message.content).startsWith(GUILDS_DATA[message.guild.id].prefix + 'barcode')) {
 
     let code = String((message.content.split(' ')[1]) || "");
     if (code=='') return;
@@ -100,8 +179,9 @@ client.on("messageCreate", async message => {
 });
 
 client.on("messageCreate", async message => {
-  if (String(message.content).startsWith(PREFIX + 'hackPetrol')) {
+  if (String(message.content).startsWith(GUILDS_DATA[message.guild.id].prefix + 'hackPetrol')) {
 
+    // emoji
     message.react('<:Profit:885976164412297226>');
 
     let number = parseInt((message.content.split(' ')[1]) || "1");
